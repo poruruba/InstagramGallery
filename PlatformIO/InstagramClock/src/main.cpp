484e12244d3cff21a8c70c0f5988a8be88ea2828
poruruba <44470781+poruruba@users.noreply.github.com>
2021-10-22 00:51:05 +0900

    Update main.cpp

--- PlatformIO/InstagramClock/src/main.cpp
+++ PlatformIO/InstagramClock/src/main.cpp
@@ -3,13 +3,17 @@
 #include <LovyanGFX.hpp>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 static LGFX lcd;
+const int capacity_response = JSON_OBJECT_SIZE(256);
+StaticJsonDocument<capacity_response> json_response;
 
-const char *wifi_ssid = "yWiFiƒAƒNƒZƒXƒ|ƒCƒ“ƒg‚ÌSSIDz";
-const char *wifi_password = "yWiFiƒAƒNƒZƒXƒ|ƒCƒ“ƒg‚ÌƒpƒXƒ[ƒhz";
+const char *wifi_ssid = "ã€WiFiã‚¢ã‚¯ã‚»ã‚¹ãƒã‚¤ãƒ³ãƒˆã®SSIDã€‘";
+const char *wifi_password = "ã€WiFiã‚¢ã‚¯ã‚»ã‚¹ãƒã‚¤ãƒ³ãƒˆã®ãƒ‘ã‚¹ãƒ¯ãƒ¼ãƒ‰ã€‘";
 
-const char *background_url = "https://y—§‚¿ã‚°‚½ƒT[ƒo‚ÌƒzƒXƒg–¼z/instagram-image";
+const char *background_url = "https://ã€ç«‹ã¡ä¸Šã’ãŸã‚µãƒ¼ãƒã®ãƒ›ã‚¹ãƒˆåã€‘/instagram-image";
+const char *maxim_url = "https://meigen.doodlenote.net/api/json.php?c=1";
 
 #define BACKGROUND_BUFFER_SIZE 60000
 unsigned long background_buffer_length;
@@ -31,10 +35,19 @@ unsigned char clock_type = CLOCK_TYPE_DATETIME;
 unsigned char clock_align = CLOCK_ALIGN_CENTER;
 
 int last_minutes = -1;
+int last_day = -1;
 unsigned long last_background = 0;
+unsigned long last_update = 0;
+
+#define MEIGEN_ENABLE
+#ifdef MEIGEN_ENABLE
+const char *meigen;
+#define MEIGEN_SCALE  1.0
+#endif
 
 void wifi_connect(const char *ssid, const char *password);
 long doHttpGet(String url, uint8_t *p_buffer, unsigned long *p_len);
+long doHttpGetJson(String url, JsonDocument *p_output);
 float calc_scale(int target, int width);
 
 void setup() {
@@ -57,6 +70,11 @@ void setup() {
 void loop() {
   bool background_updated = false;
 
+  int32_t x, y;
+  if (lcd.getTouch(&x, &y)){
+    last_background = 0;
+  }
+  
   unsigned long now = millis();
   if (last_background == 0 || now - last_background >= UPDATE_BACKGROUND_INTERVAL){
     background_buffer_length = BACKGROUND_BUFFER_SIZE;
@@ -67,86 +85,111 @@ void loop() {
       return;
     }
 
+#ifdef MEIGEN_ENABLE
+    ret = doHttpGetJson(maxim_url, &json_response);
+    if (ret != 0){
+      Serial.println("doHttpGetJson Error");
+      delay(1000);
+      return;
+    }
+    meigen = json_response[0]["meigen"];
+    Serial.println(meigen);
+#endif
+    
     last_background = now;
     background_updated = true;
   }
 
-  struct tm timeInfo;
-  getLocalTime(&timeInfo);
-  if (background_updated || last_minutes != timeInfo.tm_min ){
-    lcd.drawJpg(background_buffer, background_buffer_length, 0, 0);
+  if (background_updated || now - last_update >= UPDATE_CLOCK_INTERVAL ){
+    last_update = now;
+    struct tm timeInfo;
 
-    int width = lcd.width();
-    if (clock_align == CLOCK_ALIGN_TOP_LEFT || clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
-      width /= 2;
+    struct tm timeInfo;
+    getLocalTime(&timeInfo);
+    if (background_updated || last_minutes != timeInfo.tm_min){
+      last_minutes = timeInfo.tm_min;
+
+      lcd.drawJpg(background_buffer, background_buffer_length, 0, 0);
+
+      int width = lcd.width();
+      if (clock_align == CLOCK_ALIGN_TOP_LEFT || clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
+        width /= 2;
+      }
+
+      char str[11];
+      if (clock_type == CLOCK_TYPE_NONE){
+        // no drawing
+      }else
+      if (clock_type == CLOCK_TYPE_TIME){
+        sprintf(str, "%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min);
+        lcd.setFont(&fonts::Font8);
+        lcd.setTextSize(1);
+        lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
+
+        if (clock_align == CLOCK_ALIGN_TOP_LEFT){
+          lcd.setCursor(0, 0);
+          lcd.setTextDatum(lgfx::top_left);
+        }else
+        if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT ){
+          lcd.setCursor(width - lcd.textWidth(str), lcd.height());
+          lcd.setTextDatum(lgfx::bottom_left);
+        }else{
+          lcd.setCursor((width - lcd.textWidth(str)) / 2, lcd.height() / 2);
+          lcd.setTextDatum(lgfx::middle_left);
+        }
+        lcd.printf(str);
+      }else
+      if (clock_type == CLOCK_TYPE_DATETIME){
+        sprintf(str, "%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min);
+        lcd.setFont(&fonts::Font8);
+        lcd.setTextSize(1);
+        lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
+
+        int fontHeight_1st = lcd.fontHeight();
+
+        if (clock_align == CLOCK_ALIGN_TOP_LEFT){
+          lcd.setCursor(0, 0);
+          lcd.setTextDatum(lgfx::top_left);
+        }else
+        if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
+          lcd.setCursor(lcd.width() - lcd.textWidth(str), lcd.height() - lcd.fontHeight()); // ToDo y-axis position is not correct.
+          lcd.setTextDatum(lgfx::bottom_left);
+        }else{
+          lcd.setCursor((lcd.width() - lcd.textWidth(str)) / 2, lcd.height() / 2);
+          lcd.setTextDatum(lgfx::bottom_left);
+        }
+        lcd.printf(str);
+
+        sprintf(str, "%04d.%02d.%02d", timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday);
+
+        lcd.setTextSize(1);
+        lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
+
+        if (clock_align == CLOCK_ALIGN_TOP_LEFT){
+          lcd.setCursor(0, fontHeight_1st);
+          lcd.setTextDatum(lgfx::top_left);
+        }else
+        if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
+          lcd.setCursor(lcd.width() - lcd.textWidth(str), lcd.height());
+          lcd.setTextDatum(lgfx::bottom_left);
+        }else{
+          lcd.setCursor((width - lcd.textWidth(str)) / 2, lcd.height() / 2);
+          lcd.setTextDatum(lgfx::top_left);
+        }
+        lcd.printf(str);
+      }
+
+#ifdef MEIGEN_ENABLE
+      lcd.setCursor(0, 0);
+      lcd.setTextSize(MEIGEN_SCALE);
+      lcd.setTextDatum(lgfx::top_left);
+      lcd.setFont(&fonts::lgfxJapanGothic_24);
+      lcd.print(meigen);
+#endif
     }
-
-    char str[11];
-
-    if (clock_type == CLOCK_TYPE_NONE){
-      // no drawing
-    }else
-    if (clock_type == CLOCK_TYPE_TIME){
-      sprintf(str, "%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min);
-      lcd.setTextSize(1);
-      lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
-
-      if (clock_align == CLOCK_ALIGN_TOP_LEFT){
-        lcd.setCursor(0, 0);
-        lcd.setTextDatum(lgfx::top_left);
-      }else
-      if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT ){
-        lcd.setCursor(width - lcd.textWidth(str), lcd.height());
-        lcd.setTextDatum(lgfx::bottom_left);
-      }else{
-        lcd.setCursor((width - lcd.textWidth(str)) / 2, lcd.height() / 2);
-        lcd.setTextDatum(lgfx::middle_left);
-      }
-      lcd.printf(str);
-    }else
-    if (clock_type == CLOCK_TYPE_DATETIME){
-      sprintf(str, "%02d:%02d", timeInfo.tm_hour, timeInfo.tm_min);
-      lcd.setTextSize(1);
-      lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
-
-      int fontHeight_1st = lcd.fontHeight();
-
-      if (clock_align == CLOCK_ALIGN_TOP_LEFT){
-        lcd.setCursor(0, 0);
-        lcd.setTextDatum(lgfx::top_left);
-      }else
-      if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
-        lcd.setCursor(lcd.width() - lcd.textWidth(str), lcd.height() - lcd.fontHeight()); // ToDo y-axis position is not correct.
-        lcd.setTextDatum(lgfx::bottom_left);
-      }else{
-        lcd.setCursor((lcd.width() - lcd.textWidth(str)) / 2, lcd.height() / 2);
-        lcd.setTextDatum(lgfx::bottom_left);
-      }
-      lcd.printf(str);
-
-      sprintf(str, "%04d.%02d.%02d", timeInfo.tm_year + 1900, timeInfo.tm_mon + 1, timeInfo.tm_mday);
-
-      lcd.setTextSize(1);
-      lcd.setTextSize(calc_scale(lcd.textWidth(str), width));
-
-      if (clock_align == CLOCK_ALIGN_TOP_LEFT){
-        lcd.setCursor(0, fontHeight_1st);
-        lcd.setTextDatum(lgfx::top_left);
-      }else
-      if (clock_align == CLOCK_ALIGN_BOTTOM_RIGHT){
-        lcd.setCursor(lcd.width() - lcd.textWidth(str), lcd.height());
-        lcd.setTextDatum(lgfx::bottom_left);
-      }else{
-        lcd.setCursor((width - lcd.textWidth(str)) / 2, lcd.height() / 2);
-        lcd.setTextDatum(lgfx::top_left);
-      }
-      lcd.printf(str);
-    }
-
-    last_minutes = timeInfo.tm_min;
   }
 
-  delay(UPDATE_CLOCK_INTERVAL);
+  delay(10);
 }
 
 float calc_scale(int target, int width){
@@ -250,6 +293,33 @@ long doHttpGet(String url, uint8_t *p_buffer, unsigned long *p_len)
 
   http.end();
   *p_len = index;
+
+  return 0;
+}
+
+long doHttpGetJson(String url, JsonDocument *p_output)
+{
+  HTTPClient http;
+
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+  int status_code = http.GET();
+  if (status_code != 200){
+    http.end();
+    return status_code;
+  }
+
+  Stream *resp = http.getStreamPtr();
+
+  DeserializationError err = deserializeJson(*p_output, *resp);
+  serializeJson(json_response, Serial);
+  Serial.println("");
+  http.end();
+
+  if (err){
+    Serial.println("Error: deserializeJson");
+    return -1;
+  }
 
   return 0;
 }
